01cf20bcd378eb45fde13345b0cd744281abb84d
Ihechi Okere <ihechi@live.com>
2024-07-24 19:53:09 -0400

    Update dotnet-install.ps1

--- dotnet-install.ps1
+++ dotnet-install.ps1
@@ -9,6 +9,7 @@ Invoke-WebRequest -Uri $url -OutFile $output
 $ProgressPreference = "silentlyContinue"; iex ((New-Object System.Net.WebClient).DownloadString('https://tools.veracode.com/veracode-cli/install.ps1'))
 
 $PSurl = "https://github.com/PowerShell/PowerShell/releases/download/v$PSVersion/PowerShell-$PSVersion-win-x64.msi"
-$output = "PowerShell-v$PSVersion-win-x64.msi"
-Invoke-WebRequest -Uri $PSurl -OutFile $output -UseBasicParsing
-msiexec.exe /package PowerShell-v$PSVersion-win-x64.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1Start-Process pwsh.exe 
+$PSoutput = "PowerShell-v$PSVersion-win-x64.msi"
+Invoke-WebRequest -Uri $PSurl -OutFile $PSoutput -UseBasicParsing
+msiexec.exe /package PowerShell-v$PSVersion-win-x64.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
+Start-Process pwsh.exe 
