e8d8604c017f8f43593911de34c4da6c45f853c9
Ihechi Okere <ihechi@live.com>
2024-07-24 21:39:57 -0400

    Update dotnet-install.ps1

--- dotnet-install.ps1
+++ dotnet-install.ps1
@@ -4,8 +4,8 @@ $output = "dotnet8-install.ps1"
 $ProgressPreference = "silentlyContinue"; iex ((New-Object System.Net.WebClient).DownloadString('https://tools.veracode.com/veracode-cli/install.ps1'))
 $PSurl = "https://github.com/PowerShell/PowerShell/releases/download/v$PSVersion/PowerShell-$PSVersion-win-x64.msi"
 $PSoutput = "PowerShell-v$PSVersion-win-x64.msi"
-$JavaUrl = "https://enos.itcollege.ee/~jpoial/allalaadimised/jdk8/jdk-8u401-windows-x64.exe"
-$JavaOutput = "jdk-8u401-windows-x64.exe"
+$JavaUrl = "https://cfdownload.adobe.com/pub/adobe/coldfusion/java/java8/java8u391/jdk/jdk-8u391-windows-x64.exe"
+$JavaOutput = "jdk-8u391-windows-x64.exe"
 Invoke-WebRequest -Uri $url -OutFile $output
 ./dotnet8-install.ps1 -Channel LTS -InstallDir 'C:\Program Files\dotnet'
 ./dotnet8-install.ps1  -Channel 8.0 -Runtime windowsdesktop
@@ -14,8 +14,7 @@ Invoke-WebRequest -Uri $PSurl -OutFile $PSoutput -UseBasicParsing
 msiexec.exe /package PowerShell-v$PSVersion-win-x64.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
 sleep 120
 Invoke-WebRequest -Uri $JavaUrl -OutFile $JavaOutput -UseBasicParsing
-.\jdk-8u401-windows-x64.exe /s   
+.\jdk-8u391-windows-x64.exe /s   
 sleep 120
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Script -Name winget-install -RequiredVersion 2.1.0 -Force 
-pwsh.exe 
