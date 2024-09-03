a6725fba58275de2f137b0ca5c12167a33a8e68e
Ihechi Okere <ihechi@live.com>
2024-09-03 13:28:31 -0400

    Update linux-ado-dxp.sh

--- linux-ado-dxp.sh
+++ linux-ado-dxp.sh
@@ -8,6 +8,8 @@ sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get install -y powershell
 sudo snap install powershell --classic
 sudo apt-get install -y zip
+sudo apt install -y default-jre
+sudo apt install -y default-jdk
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm
 sudo apt-get install -y nuget
