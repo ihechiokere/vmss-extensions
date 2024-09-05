75be0cb61f676d571fa11b5a2805762085a16662
Ihechi Okere <ihechi@live.com>
2024-09-05 12:31:23 -0400

    sudo restored

--- linux-ado-dxp.sh
+++ linux-ado-dxp.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh
-bash nodesource_setup.sh 2>&1 >> /tmp/log.txt
+sudo curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh 2>&1 >> /tmp/log.txt
 ii=$(grep -w ID /etc/os-release | awk -F= '{print $2}')
 ver=$(grep -w VERSION_ID /etc/os-release | awk -F\" '{print $2}')
-wget https://packages.microsoft.com/config/$ii/$ver/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-dpkg -i packages-microsoft-prod.deb
-apt update
-apt-get install -y powershell
-snap install powershell --classic
-apt-get install -y zip
-apt install -y default-jre
-apt install -y default-jdk
-apt-get install -y nodejs
-apt-get install -y npm
-apt-get install -y nuget
-apt-get install -y dotnet-sdk-8.0
-apt-get install -y dotnet-host
-snap install --classic dotnet-sdk
-rm -rf packages-microsoft-prod.deb
-reboot
+sudo wget https://packages.microsoft.com/config/$ii/$ver/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt update
+sudo apt-get install -y powershell
+sudo snap install powershell --classic
+sudo apt-get install -y zip
+sudo apt install -y default-jre
+sudo apt install -y default-jdk
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm
+sudo apt-get install -y nuget
+sudo apt-get install -y dotnet-sdk-8.0
+sudo apt-get install -y dotnet-host
+sudo snap install --classic dotnet-sdk
+sudo rm -rf packages-microsoft-prod.deb
+sudo reboot
