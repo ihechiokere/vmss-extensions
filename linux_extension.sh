5675c20379e32d43414afda313391748b13ff158
Ihechi Okere <ihechi@live.com>
2024-09-06 10:54:49 -0400

    linux extension

--- linux_extension.sh
+++ linux_extension.sh
@@ -1,1 +1,4 @@
-
+#!/bin/bash
+curl https://raw.githubusercontent.com/ihechiokere/vmss-extensions/main/linux-ado-dxp.sh > linux-ado-dxp.sh; sudo chmod u+x linux-ado-dxp.sh; 
+sleep 30
+./linux-ado-dxp.sh
